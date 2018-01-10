9a441b7b6355c38fb35766623390390ab1fbdcc1
Alex Montgomery <Alexpm@gmail.com>
2018-01-10 12:01:31 +0800

    added vuex local storage

--- store/index.js
+++ store/index.js
@@ -1,5 +1,35 @@
+import VuexPersistence from 'vuex-persist'
+
+let vuexLocal
+
+const vuexLocalPlugin = store => {
+  // called when the store is initialized
+  if (process.browser) {
+    window.onNuxtReady(nuxt => {
+      vuexLocal = new VuexPersistence({
+        strictMode: true,
+        storage: window.localStorage
+      })
+      return (vuexLocal.plugin)(store)
+    })
+  }
+  store.subscribe((mutation, state) => {
+    if (mutation.type === 'RESTORE_MUTATION') {
+      console.log('Restoring State')
+    }
+  })
+}
+
+export const plugins = [vuexLocalPlugin]
+
 export const state = () => ({
   upcomingElections: [],
   currentRequestId: null,
   language: null
 })
+
+export const mutations = {
+  RESTORE_MUTATION (state, payload) {
+    vuexLocal.RESTORE_MUTATION(state, payload)
+  }
+}
