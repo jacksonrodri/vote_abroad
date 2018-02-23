b1f6b6b7fc985293cbf6c74e53b0e14ad54d8a63
Alex Montgomery <Alexpm@gmail.com>
2018-02-23 19:05:14 +0800

    change CT id to empty array instead of false

---
+++ pages/adm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  layout: 'blank',
+  head: {
+    script: [
+      { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
+      { src: 'https://unpkg.com/netlify-cms@^1.0.0/dist/cms.js' }
+    ],
+    link: [
+      { rel: 'stylesheet', href: 'https://unpkg.com/netlify-cms@^1.0.0/dist/cms.css' }
+    ]
+  }
+}
+</script>
