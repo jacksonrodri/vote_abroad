76c48592a76166a743e358176a675f89219aae60
Alex Montgomery <Alexpm@gmail.com>
2018-02-24 17:27:22 +0800

    pdf

--- lambda/hello.js
+++ lambda/hello.js
@@ -60,11 +60,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 19);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */
+/******/ ({
+
+/***/ 19:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -78,4 +79,5 @@ exports.handler = function (event, context, callback) {
 };
 
 /***/ })
-/******/ ])));
+
+/******/ })));
