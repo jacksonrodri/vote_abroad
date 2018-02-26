461cf13a5047d921926538db9302c0ab81a49df7
Alex Montgomery <Alexpm@gmail.com>
2018-02-26 13:56:36 +0800

    added md module

--- pages/request/_stage/index.vue
+++ pages/request/_stage/index.vue
@@ -226,9 +226,21 @@
         :toolTipTitle="`Why am I being asked this?`"
         v-model="ssn">
         <div slot="instructions">
+          <i18n path=request.id.instructionsReq2 tag="vue-markdown" :places="{ state: stateRules.state, allButLastTypes: allButLastIdType, lastType: lastIdType }">
+          </i18n>
           <p>
-            <i18n path="request.id.instructions" tag="p">
-              <span place="limit">{{ changeLimit }}</span>
+            <i18n path="request.id.reqInstructions" tag="p">
+              <span place="state">{{ stateRules.state }}</span>
+              <span place="type">
+                <span v-if="stateRules && stateRules.id && stateRules.id.length > 0">
+                  <span v-for="(idType, index) in stateRules.id" :key="idType">
+                    <span><strong>{{$t(`request.idTypes.${idType}`)}}</strong></span>
+                    <span v-if="stateRules.id.length > 1 && index === stateRules.id.length - 2"> or </span>
+                    <span v-else-if="index+1 < stateRules.id.length">, </span>
+                    <span v-else>. </span>
+                  </span>
+                </span>
+              </span>
               <a place="action" :href="changeUrl">{{ $t('change') }}</a>
             </i18n><br>
             <span v-if="stateRules && stateRules.id && stateRules.id.length === 0">Providing <strong>one</strong> of the following may help your election official find your voting record. </span>
@@ -248,10 +260,13 @@
         </div>
         <div slot="tooltip">
           <vue-markdown v-if="stateRules && stateRules.id && stateRules.id.length === 0">
-            {{$t('request.id.notRequiredTooltip', {state: stateRules.state})}}
+            {{$t('request.id.instructionsOptional', {state: stateRules.state})}}
+          </vue-markdown>
+          <vue-markdown v-else-if="stateRules && stateRules.id && stateRules.id.length === 1">
+            {{$t('request.id.instructionsReq1', {state: stateRules.state})}}
           </vue-markdown>
           <vue-markdown v-else>
-            {{$t('request.id.requiredTooltip', {state: stateRules.state})}}
+            {{$t('request.id.instructionsReq1+', {state: stateRules.state, allButLastTypes: 'a, b, c', lastType: 'd'})}}
           </vue-markdown>
           <!-- <p v-if="stateRules && stateRules.id && stateRules.id.length === 0">{{ stateRules.state }} does not require identification to register to vote.  You may provide the last 4 digits of your Social Security Number <strong>OR</strong> state ID to help your voting official find your records.</p>
           <p v-else>Identification is required by some states including {{ stateRules.state }}.  </p> -->
@@ -421,6 +436,28 @@ export default {
       } else {
         return undefined
       }
+    },
+    allButLastIdType () {
+      return this.stateRules.id.length > 2
+        ? this.stateRules.id.slice(0, this.stateRules.id.length - 1).map(x => this.$t(`request.id.${x}`)).join(', ')
+        : this.$t(`request.id.${this.stateRules.id[0]}`)
+    },
+    lastIdType () {
+      return this.$t(`request.id.${this.stateRules.id.slice(-1)[0]}`)
+    },
+    idTypesString () {
+      let arr = this.stateRules & this.stateRules.id.length > 0 ? this.stateRules.id : [this.$t('request.id.SSN4'), this.$t('request.id.StateID')]
+      return arr.map(x => this.$t(`request.id[${x}]`))
+        .map((x, i, a) => {
+          switch (a.length - i) {
+            case 1:
+              return x
+            case 2:
+              return `${x} or `
+            default:
+              return `${x}, `
+          }
+        })
     },
     maxDate () {
       return new Date(2000, 10, 6)
