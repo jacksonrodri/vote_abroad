e5688a6144739d7fa4f3d8e726e2cb46faced3bd
Alex Montgomery <Alexpm@gmail.com>
2018-03-16 01:35:10 +0800

    formatting

--- pages/_page.vue
+++ pages/_page.vue
@@ -1,15 +1,21 @@
 <template>
-<ContentContainer>
-  <div slot="lead" class="article-lead">
-    <h1 class="article-title">{{ page.title }}</h1>
+<div class="hero-body">
+  <div class="columns is-centered">
+    <div class="column is-8">
+      <ContentContainer>
+        <div slot="lead" class="article-lead">
+          <h1 class="article-title">{{ page.title }}</h1>
+        </div>
+        <section slot="content" class="article-body">
+          <div v-html="page.body"></div>
+        </section>
+        <div slot="closer" class="article-closer"><img v-if="page.image" :src="`/img/articles/${page.image}`" class="article-image"/>
+          <p v-if="page.quote" class="article-quote">{{ page.quote }}</p>
+        </div>
+      </ContentContainer>
+    </div>
   </div>
-  <section slot="content" class="article-body">
-    <div v-html="page.body"></div>
-  </section>
-  <div slot="closer" class="article-closer"><img v-if="page.image" :src="`/img/articles/${page.image}`" class="article-image"/>
-    <p v-if="page.quote" class="article-quote">{{ page.quote }}</p>
-  </div>
-</ContentContainer>
+</div>
 </template>
 
 <script>
