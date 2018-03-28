f51834c954b1ab0c445520eac373cf3706f140ea
Alex Montgomery <Alexpm@gmail.com>
2018-03-28 08:37:22 +0800

    fix state generate

---
+++ pages/states/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 class="title">Voting from abroad in your State</h1>
+</template>
+<script>
+export default {
+  asyncData: async ({ app }) => ({
+    page: await app.$content(`${app.i18n.locale}/pages`).get(`${app.i18n.locale !== app.i18n.defaultLocale ? app.i18n.locale + '/' : ''}states`)
+  })
+}
+</script>
--- pages/states/notindex.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1 class="title">Voting from abroad in your State</h1>
-</template>
