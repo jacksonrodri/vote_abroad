ea34c1ba36879a822304268eb275b01a7be4852a
Alex Montgomery <Alexpm@gmail.com>
2018-04-01 00:36:34 +0800

    signature changes

--- pages/request/sign-and-submit/index.vue
+++ pages/request/sign-and-submit/index.vue
@@ -3,6 +3,17 @@
     <!-- hello from sign index
     <nuxt-link to="sign">sign</nuxt-link>
     <nuxt-link to="sign2">sign2</nuxt-link> -->
+    <sign></sign>
     &nbsp;
   </div>
 </template>
+
+<script>
+import Sign from '~/components/sign3'
+
+export default {
+  components: {
+    Sign
+  }
+}
+</script>
