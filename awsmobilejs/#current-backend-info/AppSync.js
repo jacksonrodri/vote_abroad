3ddf6a0fb4f444d11ba22cb3652e8f436cd3b990
Alex Montgomery <Alexpm@gmail.com>
2018-04-23 20:10:35 +0800

    vt rules

---
+++ awsmobilejs/#current-backend-info/AppSync.js
@@ -0,0 +1,5 @@
+export default {
+	"graphqlEndpoint": "https://dm6oltcfj5dxraq6st7iyrcvby.appsync-api.us-east-1.amazonaws.com/graphql",
+	"region": "us-east-1",
+	"authenticationType": "AWS_IAM",
+}
