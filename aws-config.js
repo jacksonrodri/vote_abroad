cd8b692b688fc3859dd53705f8ceaf2537c98fd2
Alex Montgomery <Alexpm@gmail.com>
2018-04-26 18:43:25 +0800

    upgrade nuxtent and test

--- aws-config.js
+++ aws-config.js
@@ -25,10 +25,10 @@ var aws_user_pools_id = 'us-east-1_aXuRvIHe4';
 var aws_user_pools_mfa_type = 'ON';
 var aws_user_pools_web_client_id = '5ehiffe6s5stq8hpf401k7096a';
 
-AWS.config.region = aws_project_region;
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: aws_cognito_identity_pool_id
-  }, {
-    region: aws_cognito_region
-  });
-AWS.config.update({customUserAgent: 'MobileHub v0.1'});
+// AWS.config.region = aws_project_region;
+// AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+//     IdentityPoolId: aws_cognito_identity_pool_id
+//   }, {
+//     region: aws_cognito_region
+//   });
+// AWS.config.update({customUserAgent: 'MobileHub v0.1'});
