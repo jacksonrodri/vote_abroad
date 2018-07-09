0981129df5d77d0955db384ae1f72572e4e15f47
Alex Montgomery <Alexpm@gmail.com>
2018-07-09 19:41:21 +0800

    county fix

--- components/VotingAddress.vue
+++ components/VotingAddress.vue
@@ -208,7 +208,7 @@ export default {
         this.$store.commit('requests/update', {leo: null})
       }
       if (!oldVal || newVal !== oldVal) {
-        this.county = null
+        this.county = ''
         this.$store.commit('requests/update', {
           stateSpecial: null,
           identification: null
