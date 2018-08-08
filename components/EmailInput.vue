10a87e378a769f572fecae8b534bdb37193bf945
Alex Montgomery <Alexpm@gmail.com>
2018-08-08 18:07:27 -0400

    emailinput component

---
+++ components/EmailInput.vue
@@ -0,0 +1,56 @@
+<template>
+<div class="field">
+  <basic-label :fieldName="fieldName"></basic-label>
+  <b-field
+    v-if="typeof fieldValue === 'string' || fieldValue === null || fieldValue === undefined"
+    :type="fieldType"
+    :messages="fieldMessages">
+    <!-- :message="v.$error ? Object.entries(v).filter(([key, value]) => key.charAt(0) !== '$' && value === false).map(x => $t(`request.${fieldName}.messages.${x[0]}`)) : '' "> -->
+    <b-field
+      :type="fieldType">
+      <a @click.prevent="$el.querySelector('input').focus()"
+        :class="['button', 'is-static', 'control']">
+        <b-icon
+          pack="fas"
+          icon="at">
+        </b-icon>
+      </a>
+      <b-input v-model="fieldValue"
+        expanded
+        :class="requiredClass"
+        :id="fieldName"
+        :placeholder="placeholderAddress"
+        :autocomplete="autoComplete"
+        :maxlength="maxLength"
+        @input="() => { $emit('input'); $emit('delayTouch', v) }"
+        :ref="fieldName"></b-input>
+    </b-field>
+  </b-field>
+  <span v-if="mailcheck && mailcheck !== fieldValue.toLowerCase()" class="help is-vfa">Did you mean <a @click="fieldValue = mailcheck"><span class="has-text-primary">{{ mailcheck }}</span></a>?</span>
+</div>
+</template>
+
+<script>
+import { mapMutations, mapGetters } from 'vuex'
+import fieldMixin from '~/mixins/fieldMixin.js'
+import { randomPresAddress } from '~/utils/helpers'
+
+export default {
+  name: 'EmailInput',
+  mixins: [fieldMixin],
+  computed: {
+    placeholderAddress () { return `e.g. ${randomPresAddress()}` },
+    mailcheck () { return this.mailCheck(this.fieldValue) },
+    ...mapGetters('requests', ['getCurrent']),
+    ...mapGetters('data', ['mailCheck'])
+  },
+  methods: {
+    ...mapMutations('requests', ['update'])
+  }
+
+}
+</script>
+
+<style>
+
+</style>
