2dfc72cc8fbe07254972e70e8419814a59cbd7c5
Alex Montgomery <Alexpm@gmail.com>
2018-08-08 18:11:04 -0400

    basic label

---
+++ components/BasicLabel.vue
@@ -0,0 +1,28 @@
+<template>
+  <label :for="fieldName" class="label">
+    {{$t(`request.${fieldName}.label`)}}
+    <a v-if="$te(`request.${fieldName}.tooltip`)" @click="$emit('toggleInfo')" class="button is-white is-small">
+      <span>
+        <b-icon
+          type="is-info"
+          icon="info-circle"></b-icon>
+      </span>
+    </a>
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    fieldName: {
+      type: String,
+      required: true
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
