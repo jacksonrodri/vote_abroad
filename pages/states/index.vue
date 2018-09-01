1f710579cfe5e47e7e2c6c3b0e8663e2c63def2a
Alex Montgomery <Alexpm@gmail.com>
2018-09-01 12:21:02 +0800

    really fixed

--- pages/states/index.vue
+++ pages/states/index.vue
@@ -27,8 +27,8 @@ export default {
     page: await app.$content(`${app.i18n.locale}/pages`).get(`${app.i18n.locale !== app.i18n.defaultLocale ? app.i18n.locale + '/' : ''}states`),
     states: (await app.$content('/rls').getAll())
       .sort((a, b) => {
-        if (a.state.toUpperCase() < b.state.toUpperCase()) { return -1 }
-        if (a.state.toUpperCase() > b.state.toUpperCase()) { return 1 }
+        if (a.title.toUpperCase() < b.title.toUpperCase()) { return -1 }
+        if (a.title.toUpperCase() > b.title.toUpperCase()) { return 1 }
         return 0
       })
   }),
