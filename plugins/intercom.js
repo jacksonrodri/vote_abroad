d283a2f375900ec6e7df323feb3060d6ab52188f
Alex Montgomery <Alexpm@gmail.com>
2018-10-03 15:07:18 +0800

    fix(intercom): change app id based on environment

--- plugins/intercom.js
+++ plugins/intercom.js
@@ -1,4 +1,4 @@
 import Vue from 'vue'
 import VueIntercom from 'vue-intercom'
 
-Vue.use(VueIntercom, { appId: 'h27cpfij' })
+Vue.use(VueIntercom, { appId: process.env.dev === 'dev' ? 'h27cpfij' : 't392eszv' })
