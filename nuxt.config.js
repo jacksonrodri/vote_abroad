5c6e9cc5f99300a3c192a7f7303e139a54c2349e
Alex Montgomery <Alexpm@gmail.com>
2018-10-24 08:40:46 +0800

    linkers add vfa.org

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,7 +67,7 @@ module.exports = {
   ],
   'google-analytics': {
     id: 'UA-126220374-1',
-    linkers: ['beta.votefromabroad.org', 'students.votefromabroad.org', 'es.votefromabroad.org', 'vr.votefromabroad.org'],
+    linkers: ['beta.votefromabroad.org', 'students.votefromabroad.org', 'es.votefromabroad.org', 'vr.votefromabroad.org', 'votefromabroad.org'],
     set: [
       { field: 'dimension1', value: process.env.BRANCH || 'dev' }
     ],
